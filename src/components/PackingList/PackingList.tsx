467ad4d66f9514816a6b9c4b8e1ca2fc9a1aa668
William Fernandes Dias <william.winchester1967@gmail.com>
2023-10-19 20:23:40 -0300

    feat: implement user defined sorting method

--- src/components/PackingList/PackingList.tsx
+++ src/components/PackingList/PackingList.tsx
@@ -1,15 +1,51 @@
 import PackingItem from '@/components/PackingItem';
 import { usePackingItems } from '@/contexts/packingItems';
+import { useState } from 'react';
 
 export default function PackingList() {
-  const { items } = usePackingItems();
+  const { items, dispatch } = usePackingItems();
+  const [sortBy, setSortBy] = useState<keyof FarAway.PackingItem>('id');
+
+  let sortedItems: FarAway.PackingItem[];
+  switch (sortBy) {
+    case 'id':
+      sortedItems = [...items].sort((a, b) => a.id - b.id);
+      break;
+    case 'description':
+      sortedItems = [...items].sort((a, b) =>
+        a.description.localeCompare(b.description),
+      );
+      break;
+    case 'packed':
+      sortedItems = [...items].sort(a => (a.packed ? -1 : 1));
+      break;
+    default:
+      sortedItems = [...items];
+      break;
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <PackingItem key={item.id} item={item} />
         ))}
       </ul>
+      <div className="actions">
+        <select
+          name="sortBy"
+          id="sort-by"
+          value={sortBy}
+          placeholder="Sort by"
+          onChange={e =>
+            setSortBy(e.target.value as keyof FarAway.PackingItem)
+          }>
+          <option value="id">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={() => dispatch({ type: 'clear' })}>Clear list</button>
+      </div>
     </div>
   );
 }
