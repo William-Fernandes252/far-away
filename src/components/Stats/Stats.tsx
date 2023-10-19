f8c1a12eb6fd74afdec83c0b2e95bfbd68e1756a
William Fernandes Dias <william.winchester1967@gmail.com>
2023-10-19 19:38:31 -0300

    feat: improve stats by adding messages for 0 and 100 percentage

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -6,12 +6,19 @@ export default function Stats() {
   const packedPercentage = Math.round(
     (packedCount / (items.length || 1)) * 100,
   );
+
+  let footerContent: string;
+  if (items.length === 0) {
+    footerContent = 'Start adding some items to your packing list 🚀';
+  } else if (packedPercentage === 100) {
+    footerContent = 'You got everthing! Ready to go ✈️';
+  } else {
+    footerContent = `👜 You have ${items.length} items on your list, and you already packed ${packedCount} (${packedPercentage}%)`;
+  }
+
   return (
     <footer className="stats">
-      <em>
-        👜 You have {items.length} items on your list, and you already packed{' '}
-        {packedCount} ({packedPercentage}%)
-      </em>
+      <em>{footerContent}</em>
     </footer>
   );
 }
