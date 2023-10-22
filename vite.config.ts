b8b9f5a66554f2734544600762e5bba9f1e6dd6c
William Fernandes Dias <william.winchester1967@gmail.com>
2023-10-22 17:34:38 -0300

    test: add test configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -5,4 +7,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    includeSource: ['src/**/*.{jsx,tsx,js,ts}'],
+    globals: true,
+    environment: 'jsdom',
+  },
 });
